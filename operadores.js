d656308b2680eb685de9a89b66f944b803753cce
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-04-02 18:12:52 -0300

    Operadores JS

---
+++ operadores.js
@@ -0,0 +1,19 @@
+const resultado = 7*3;
+const resultadoDivisão = 9/3;
+const resultadoCubo = 2**3;
+const restoDivisão = 28%7;
+
+let x = 27;
+
+x++;
+x--;
+
+
+//concatenação
+console.log(x);
+
+const texto1 = "Olá,";
+const texto2 = "mundo!";
+
+const textoFinal = texto1 + texto2;
+console.log(textoFinal);
