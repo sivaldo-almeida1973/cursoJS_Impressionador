0ebf6b6ac5f17e1e08e19e89bce634094626c522
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-04-02 17:52:42 -0300

    Iniciado curso JavaScript impressionador

---
+++ primeiroCodigo.js
@@ -0,0 +1,12 @@
+let mensagem = 'Ola mundo, estou estudando JavaScript';
+
+
+console.log(mensagem);
+console.log(mensagem);
+console.log(mensagem);
+console.log(mensagem);
+console.log(mensagem);
+
+mensagem = 'Agora ja fiz meu primeiro codigo!';
+
+console.log(mensagem);
